bc81b4b5a6444bdd2bd1676c9205382facc8a28a
Joe <joehewett1@gmail.com>
2024-12-03 17:56:21 +0000

    add particles component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import Content from './Content';
+import Stars from './Stars';
 
 const Home = () => {
   return (
     <div className="font-serif max-w-6xl mx-auto">
-      <Content />
+      {/* <Content /> */}
+      <Stars />
     </div >
   );
 };
